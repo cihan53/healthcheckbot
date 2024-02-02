884b73ce7e5778ea45ccd2ed7a0c556af13e82d0
Cihan <cihan.ozturk@hubbox.io>
2024-02-02 15:52:04 +0300

    added new log output, sends http rest api ```yaml   rest:     provider: custom.outputs.RestOutput #    silent: True     contentType: text/html     rest_host: https://status.hubbox.io/ping/xxxx/{watcher_name}
    ```

--- custom/outputs.py
+++ custom/outputs.py
@@ -13,21 +13,19 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import logging
 import collections
-import time
-import json
+import concurrent.futures
 import logging
+import re
+from urllib.parse import urlparse
+
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-import asyncio
-import concurrent.futures
+
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
 
 from healthcheckbot.common import validators
-from healthcheckbot.common.error import ConfigValidationError
 from healthcheckbot.common.model import (
     OutputModule,
     WatcherModule,
@@ -35,33 +33,36 @@ from healthcheckbot.common.model import (
     ParameterDef,
 )
 
+logger1 = logging.getLogger("RestOutput")
 
-logger1 = logging.getLogger("App")
 
 class CustomHttpHandler(logging.Handler):
-    def __init__(self, url: str, token: str, silent: bool = True,ContentType = "application/json"):
-        '''
+    def __init__(self, url: str, token: str, silent: bool = True, contentType="application/json"):
+        """
         Initializes the custom http handler
         Parameters:
             url (str): The URL that the logs will be sent to
             token (str): The Authorization token being used
-            silent (bool): If False the http response and logs will be sent 
+            silent (bool): If False the http response and logs will be sent
                            to STDOUT for debug
-        '''
+        """
         self.url = url
         self.token = token
         self.silent = silent
-        self.ContentType = ContentType
+        self.ContentType = contentType
 
         # sets up a session with the server
         self.MAX_POOLSIZE = 100
         self.session = session = requests.Session()
         session.headers.update({
-            'Host': 'machinemonitor.io',
+            'Host': urlparse(url).netloc,
             'Content-Type': self.ContentType,
-            'Authorization': 'Bearer %s' % (self.token)
+
         })
-    
+
+        if self.token:
+            session.headers.update({'Authorization': 'Bearer %s' % (self.token)})
+
         self.session.mount('https://', HTTPAdapter(
             max_retries=Retry(
                 total=5,
@@ -71,7 +72,7 @@ class CustomHttpHandler(logging.Handler):
             pool_connections=self.MAX_POOLSIZE,
             pool_maxsize=self.MAX_POOLSIZE
         ))
-        
+
         super().__init__()
 
     def emit(self, record):
@@ -81,19 +82,33 @@ class CustomHttpHandler(logging.Handler):
         Parameters:
             record: a log record
         '''
-        
+
         executor.submit(actual_emit, self, record)
 
+
 def actual_emit(self, record):
-    
     logEntry = self.format(record)
-    response = self.session.post(self.url, data=logEntry)
- 
+    response = self.session.post(replace_keywords(self.url, {"watcher_name": record.watcher_name}), data=logEntry)
+
     if not self.silent:
         print(logEntry)
         print(response.content)
-      
- 
+
+
+def replace_keywords(input_string, replacement_dict):
+    # Süslü parantez içindeki keyword'leri bulmak için regex deseni
+    pattern = r'\{(\w+)\}'
+
+    def replace(match):
+        keyword = match.group(1)
+        # Eğer keyword, replacement_dict içinde varsa, değeri ile değiştir
+        return replacement_dict.get(keyword, match.group(0))
+
+    # re.sub() fonksiyonunu kullanarak değiştirme işlemi
+    result_string = re.sub(pattern, replace, input_string)
+    return result_string
+
+
 class RestOutput(OutputModule):
     def __init__(self, application):
         super().__init__(application)
@@ -101,7 +116,7 @@ class RestOutput(OutputModule):
         self.rest_host = None  # type: str
         self.rest_token = None  # type: str
         self.facility = "healthcheck"
-        self.contentType = "application/json"
+        self.content_type = "application/json"
         self.silent = True
         self.extra_fields = {}
 
@@ -119,31 +134,25 @@ class RestOutput(OutputModule):
         self.rest_logger = logging.getLogger("Rest")
         self.rest_logger.setLevel(logging.DEBUG)
         self.rest_logger.propagate = False
-        
+
         handler = CustomHttpHandler(
             url=self.rest_host,
             token=self.rest_token,
             silent=True,
-            contentType="application/json"
+            contentType=self.content_type
         )
 
         handler.level = logging.DEBUG
         self.rest_logger.addHandler(handler)
 
-       
-
     def output(self, watcher_instance: WatcherModule, watcher_result: WatcherResult):
-        data = watcher_result.to_dict()
-      
         data = self.__flatten(watcher_result.to_dict())
         data.update(dict(tags="healthcheck", watcher_name=watcher_instance.name))
-        
- 
-            
         if len(watcher_result.extra.keys()) > 0 and "extra" in data:
             data.update(self.__flatten(watcher_result.extra))
             del data["extra"]
         data.update(self.extra_fields)
+
         self.rest_logger.info(
             "HealthcheckBot {}: Watcher {} - checks {}".format(
                 self.get_application_manager().get_instance_settings().id,
@@ -155,10 +164,9 @@ class RestOutput(OutputModule):
 
     PARAMS = (
         ParameterDef("rest_host", is_required=True),
-        ParameterDef("rest_token", is_required=True),
+        ParameterDef("rest_token", validators=(validators.string,)),
         ParameterDef("facility", validators=(validators.string,)),
         ParameterDef("silent", validators=(validators.boolean,)),
-        ParameterDef("contentType", validators=(validators.string,)),
+        ParameterDef("content_type", validators=(validators.string,)),
         ParameterDef("extra_fields", validators=(validators.dict_of_strings,)),
     )
-
